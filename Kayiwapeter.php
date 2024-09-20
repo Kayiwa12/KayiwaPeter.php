44e48a8da16662cdcd2902a878c8448a53c0725e
Kayiwa12 <peterkayiwa80@gmail.com>
2024-09-20 09:08:25 +0300

    Kayiwapeter.php

--- Kayiwapeter.php
+++ Kayiwapeter.php
@@ -16,3 +16,25 @@
     echo "Welcome!! Dear " . $TheName . "\n"; 
     echo "You are " . $Age . " Years Old Now."; // Print calculated age
 ?>
+
+//we can also add userinput for instance 
+
+<?php 
+    // We can also add user input to allow different users to put in this info then it is displayed as output
+    $TheName = readline("Enter your name: "); 
+    $HomeAddress = readline("Enter your home address: "); 
+    $DateOfBirth = readline("Enter your date of birth (DD-MM-YYYY): ");
+    
+    $CurrentYear = readline("Enter the current year: "); 
+    $BirthYear = readline("Enter your year of birth: "); 
+
+    $Age = $CurrentYear - $BirthYear;
+
+    echo "The Name is " . $TheName . ".\n"; 
+    echo "The Date of Birth is " . $DateOfBirth . ".\n"; 
+    echo "The Home Address is " . $HomeAddress . ".\n"; 
+    echo "Welcome!! Dear " . $TheName . ".\n"; 
+    echo "You are " . $Age . " years old now.\n"; 
+
+    // You can also use functions to come up with this; however, I am still understanding the concept, so the function example is not included.
+?>
